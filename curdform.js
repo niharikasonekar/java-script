96059563e9c15cd4362af4f20acc6da3b4d2d858
Niharika <niharika151196@gmail.com>
2025-05-05 22:33:01 +0530

    curd form

---
+++ curdform.js
@@ -0,0 +1,21 @@
+let Book=()=>{
+
+
+let name=document.querySelector("name").value
+let age=document.querySelector("name").value
+let checkin=document.querySelector("name").value
+let checkout=document.querySelector("name").value
+let city=document.querySelector("name").value
+let person=document.querySelector("name").value
+
+let url="http://localhost:3000/hotel"
+
+
+fetch(url,{
+    
+    method:"POST",
+    headers:{
+        "content-type 
+    },
+})
+}
